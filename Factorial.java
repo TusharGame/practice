af1ee34dbf1e0e1ddf0b984e9b6652bc3556aee0
TusharGame <113577713+TusharGame@users.noreply.github.com>
2022-09-15 21:02:17 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+package method;
+
+public class Factorial {
+public static void main(String []args) {
+	Factorial f1=new Factorial();
+	f1.findFactorial(5);
+}
+public void findFactorial(int no) {
+	int fact=1; 
+	for(int i=1; i<=no;i++) {
+		fact=fact*i;
+	}
+	System.out.print("fact of"+no+"is="+fact);
+}
+}
